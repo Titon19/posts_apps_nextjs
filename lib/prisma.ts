93eb53f20841fdf091c40bd2960dda26230bdbe6
Titon Meisya Kresna <titonkresna19@gmail.com>
2024-10-13 02:36:28 +0700

    ubah prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = global.prisma || new PrismaClient();
+// Tentukan Prisma Client sebagai bagian dari global object agar bisa digunakan ulang
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// Cek apakah instance Prisma sudah ada di global
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+  globalForPrisma.prisma = prisma; // Hanya set di non-production environment
 }
 
 export default prisma;
