1887b839aabc0b0410db2d96e51f145634acb1f1
Titon Meisya Kresna <104065865+Titon19@users.noreply.github.com>
2024-10-21 14:16:05 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get("next-auth.session-token");
+  const token = req.cookies.get("__Secure-next-auth.session-token");
   if (!token) {
     return NextResponse.redirect(new URL("/api/auth/signin", req.url));
   }
